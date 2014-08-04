f4c5581aeffbe4fe6b9b565ff7e6de4196e10dab
bassounds <ashley.doane@gmail.com>
2014-08-04 02:43:03 -0400

    improved parse function

--- uniprotAD.py
+++ uniprotAD.py
@@ -1,7 +1,3 @@
-"""
-python script retrieves uniprot sequence and metadata for list of uniprot ids
-
-"""
 import requests
 import sys
 import argparse
@@ -9,57 +5,59 @@ import argparse
 url = 'http://www.uniprot.org/'
 
 
+
+def get_parts(uni_txt):
+    for line in uni_txt.splitlines():
+        k, v = [line[:5].strip(), line[5:].strip()]
+        return k, v.split()
+
 def parse_uniprot(uni_txt):
-    """
-    Parses the text of metadata retrieved from uniprot.org.
+    """Parse metadata text from uniprot.org
 
-    Only a few fields have been parsed, but this provides a
-    template for the other fields.
-
-    A single description is generated from joining alternative
-    descriptions.
-
-    Returns a dictionary with the main UNIPROT ACC as keys.
-    """
-
-    tag = None
+     Returns a dictionary with the UNIPROT ACC as keys.
+     """
     uniprot_id = None
-    metadata_by_seqid = {}
+    metadata_by_acc = {}
+    tag = None
+    #with get_parts
     for l in uni_txt.splitlines():
         ttag = l[:5].strip()
         if ttag and ttag != tag:
             tag = ttag
-        line = l[5:].strip()
-        words = line.split()
+        words = (l[5:].strip()).split()
         if tag == "ID":
             uniprot_id = words[0]
             is_reviewed = words[1].startswith('Reviewed')
             length = int(words[2])
-            metadata_by_seqid[uniprot_id] = {
+            metadata_by_acc[uniprot_id] = {
                 'id': uniprot_id,
                 'is_reviewed': is_reviewed,
                 'length': length,
                 'sequence': '',
                 'accs': [],
                 'gene': [],
+                'refseq': []
             }
-            entry = metadata_by_seqid[uniprot_id]
+            entry = metadata_by_acc[uniprot_id]
+
         if tag == "SQ":
             if words[0] != "SEQUENCE":
                 entry['sequence'] += ''.join(words)
+
         if tag == "AC":
             accs = [w.replace(";", "") for w in words]
             entry['accs'].extend(accs)
+
         if tag == "GN":
             if 'gene' not in entry:
                 entry['gene'] = []
-                parts = words[0].split("=")
-                entry['gene'].append(parts[1])
+            parts = words[0].split("=")
+            entry['gene'].append(parts[1])
 
         if tag == "DR":
             if 'PDB' in words[0]:
                 if 'pdb' not in entry:
-                    entry['pdb'] = words[1][:-1]
+                    entry['pdb'] = [words[1][:-1]]
                 if 'pdbs' not in entry:
                     entry['pdbs'] = []
                 entry['pdbs'].append(words[1][:-1])
@@ -68,47 +66,8 @@ def parse_uniprot(uni_txt):
                     entry['refseq'] = []
                 ids = [w[:-1] for w in words[1:]]
                 entry['refseq'].extend(ids)
-            if 'KEGG' in words[0]:
-                if 'kegg' not in entry:
-                    entry['kegg'] = []
-                ids = [w[:-1] for w in words[1:]]
-                ids = filter(lambda w: len(w) > 1, ids)
-                entry['kegg'].extend(ids)
-            if 'GO' in words[0]:
-                if 'go' not in entry:
-                    entry['go'] = []
-                entry['go'].append(' '.join(words[1:]))
-            if 'Pfam' in words[0]:
-                if 'pfam' not in entry:
-                    entry['pfam'] = []
-                entry['pfam'].append(words[1][:-1])
 
-        if tag == "OS":
-            if 'organism' not in entry:
-                entry['organism'] = ""
-            entry['organism'] += line
-    #     if tag == "DE":
-    #         if 'descriptions' not in entry:
-    #             entry['descriptions'] = []
-    #         entry['descriptions'].append(line)
-    #     if tag == "CC":
-    #         if 'comment' not in entry:
-    #             entry['comment'] = ''
-    #         entry['comment'] += line + '\n'
-
-    # for entry in metadata_by_seqid.values():
-    #     descriptions = entry['descriptions']
-    #     for i in reversed(range(len(descriptions))):
-    #         description = descriptions[i]
-    #         if 'Short' in description or 'Full' in description:
-    #             j = description.find('=')
-    #             descriptions[i] = description[j + 1:].replace(';', '')
-    #         else:
-    #             del descriptions[i]
-    #     entry['description'] = '; '.join(descriptions)
-
-    return metadata_by_seqid
-
+    return metadata_by_acc
 
 
 def pull_uni(ids, format='txt'):
@@ -136,21 +95,9 @@ def pull_uni(ids, format='txt'):
 
 
 import sys
+import pprint
 
 if __name__ == '__main__':
     ids = ['P01116', 'P15056']
     res = pull_uni(ids)
-    #sys.stdout = open('log.txt', 'a')
-    F = open('resfile.pkl', 'wb')
-    import pickle
-    pickle.dump(res, F)
-    F.close()
-    import json
-    R = json.dumps(res)
-    json.dump(res, fp=open('res4json.txt', 'w'), indent=4)
-    json.dump(res, fp=open('res6json.json', 'w'), indent=4)
-    print json.dumps(res, sort_keys=True, indent=4, separators=(',', ': '))
-
-
-
-    #print >> afile, res
+    pprint.pprint(res)
